6903350bf26b1c661d30aa4cb76acfe988968e9d
Whittney Davis <wcdavis89@yahoo.com>
2019-11-14 00:26:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 def prime?(i)
-   n = 2
-  while n < i
-    return false if i % n == 0
-    n += 1
+ return self >= 2 if self <= 3
+    return true if self == 5
+    return false unless 30.gcd(self) == 1
+    (7..Integer.sqrt(self)).step(30) do |p|
+      return false if
+        self%(p)    == 0 || self%(p+4)  == 0 || self%(p+6)  == 0 || self%(p+10) == 0 ||
+        self%(p+12) == 0 || self%(p+16) == 0 || self%(p+22) == 0 || self%(p+24) == 0
+    end
+    true
   end
-  true
-end
